39c49f57bb5452b2f78ec716b9d225340f708d4f
James <ball.j.work@gmail.com>
2021-08-16 12:29:34 +0100

    search function fixed, accepts upper and lower case arguments

--- Resources/script.js
+++ Resources/script.js
@@ -2,9 +2,11 @@
 // filter events function
  function filterEvents (filter) {
     let eventsList = document.getElementById('events_Container').getElementsByClassName('event');
+    let filterToLowerCase = filter.toLocaleLowerCase();
     for (let i = 0; i < eventsList.length; i++ ) {
+        let classStr = eventsList[i].className.toLocaleLowerCase();
         if (filter == "showAll") eventsList[i].style.display = 'flex';
-        else if (eventsList[i].className.indexOf(filter) > -1) eventsList[i].style.display ='flex';
+        else if (classStr.indexOf(filterToLowerCase) > -1) eventsList[i].style.display ='flex';
         else eventsList[i].style.display = 'none';
     };
  };
@@ -17,6 +19,6 @@ arrBtn.forEach( button => {
 // search bar event handler
 let searchBar = document.getElementById('SearchBar');
 searchBar.addEventListener('keyup', () => {
-    let searchFilter= document.getElementById('SearchBar').value.toLowerCase();
+    let searchFilter= document.getElementById('SearchBar').value;
     filterEvents(searchFilter);
 });
